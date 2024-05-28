917e15a9da81a724063bfcfcd90df3440d5c7b41
Toshiaki Maki <makingx@gmail.com>
2024-05-28 23:32:10 +0900

    Add clear logs api

--- src/main/java/am/ik/lognroll/logs/QueryController.java
+++ src/main/java/am/ik/lognroll/logs/QueryController.java
@@ -6,8 +6,12 @@ import java.util.List;
 import am.ik.lognroll.logs.LogQuery.Cursor;
 import am.ik.lognroll.logs.filter.FilterExpressionTextParser;
 import am.ik.pagination.CursorPageRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +21,15 @@ public class QueryController {
 
 	private final LogQuery logQuery;
 
+	private final LogStore logStore;
+
 	private final FilterExpressionTextParser parser = new FilterExpressionTextParser();
 
-	public QueryController(LogQuery logQuery) {
+	private final Logger logger = LoggerFactory.getLogger(QueryController.class);
+
+	public QueryController(LogQuery logQuery, LogStore logStore) {
 		this.logQuery = logQuery;
+		this.logStore = logStore;
 	}
 
 	@GetMapping(path = "/api/logs")
@@ -38,6 +47,13 @@ public class QueryController {
 			searchRequest.filterExpression(this.parser.parse(filter));
 		}
 		return this.logQuery.findLatestLogs(searchRequest.build());
+	}
+
+	@DeleteMapping(path = "/api/logs")
+	public ResponseEntity<Void> clearLogs() {
+		logger.info("Clear logs!!!");
+		this.logStore.clear();
+		return ResponseEntity.noContent().build();
 	}
 
 }
