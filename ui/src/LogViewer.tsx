14f95bd3b0c06eeb53e8fc7a950e1e750f37387a
Toshiaki Maki <makingx@gmail.com>
2024-06-20 19:09:34 +0900

    show logs since 2 days ago by default

--- ui/src/LogViewer.tsx
+++ ui/src/LogViewer.tsx
@@ -86,13 +86,23 @@ interface Message {
     status: MessageStatus;
 }
 
+const formatDate = (date: Date) => {
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+    const seconds = ('0' + date.getSeconds()).slice(-2);
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
+
 const LogViewer: React.FC = () => {
     const [logs, setLogs] = useState<Log[]>([]);
-    const [count, setCount] = useState<number|string>();
+    const [count, setCount] = useState<number | string>();
     const [query, setQuery] = useState<string>('');
     const [filter, setFilter] = useState<string>('');
     const [size, setSize] = useState<number>(30);
-    const [from, setFrom] = useState<string>('');
+    const [from, setFrom] = useState<string>(formatDate(new Date(new Date().getTime() - 2 * 24 * 60 * 60 * 1000)));
     const [to, setTo] = useState<string>('');
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [jsonToTable, setJsonToTable] = useState<boolean>(false);
