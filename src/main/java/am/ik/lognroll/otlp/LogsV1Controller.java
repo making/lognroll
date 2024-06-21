478051e73fc0571bb7b3ea2bf33f70b550924bb2
Toshiaki Maki <makingx@gmail.com>
2024-06-21 17:04:42 +0900

    Add counter

--- src/main/java/am/ik/lognroll/otlp/LogsV1Controller.java
+++ src/main/java/am/ik/lognroll/otlp/LogsV1Controller.java
@@ -6,6 +6,8 @@ import am.ik.lognroll.logs.Log;
 import am.ik.lognroll.logs.LogStore;
 import am.ik.lognroll.logs.Logs;
 import com.google.protobuf.InvalidProtocolBufferException;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.opentelemetry.proto.logs.v1.LogsData;
 
 import org.springframework.http.MediaType;
@@ -18,14 +20,18 @@ public class LogsV1Controller {
 
 	private final LogStore logStore;
 
-	public LogsV1Controller(LogStore logStore) {
+	private final MeterRegistry registry;
+
+	public LogsV1Controller(LogStore logStore, MeterRegistry registry) {
 		this.logStore = logStore;
+		this.registry = registry;
 	}
 
 	@PostMapping(path = "/v1/logs",
 			consumes = { MediaType.APPLICATION_PROTOBUF_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public void logs(@RequestBody LogsData logs) throws InvalidProtocolBufferException {
 		List<Log> data = Logs.from(logs);
+		Counter.builder("logs.ingested").register(this.registry).increment(data.size());
 		this.logStore.addAll(data);
 	}
 
